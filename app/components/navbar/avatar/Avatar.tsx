acf665c62dc9816c25d7a1b8aabf88b61d647681
MrColaCandy <MahmoodKhdadady10@gmail.com>
2023-05-16 17:49:53 +0200

    initial Commit

---
+++ app/components/navbar/avatar/Avatar.tsx
@@ -0,0 +1,17 @@
+"use clint";
+import React from "react";
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      className=" rounded-full"
+      src={"/images/placeholder.jpg"}
+      alt={"Avatar"}
+      height={30}
+      width={30}
+    />
+  );
+};
+
+export default Avatar;
